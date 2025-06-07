2ae82be5ed5807d45c75727d2e778af49dd95096
ArjunPathania <arjunpathania786f@gmail.com>
2025-06-08 01:17:47 +0530

    Remove build directory from tracking

--- build/CMakeFiles/micronet.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmicronet.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arjunpathania/github/micronet++
-# Build directory: /home/arjunpathania/github/micronet++/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/arjunpathania/github/micronet++/build/micronet_tests[1]_include.cmake")
